a1bc2e67c94b6c33f2237a858e42d771da336875
muha.io <zhanmuha01@gmail.com>
2021-11-29 22:23:47 +0600

    solved 6kyu problem

---
+++ 6kyu/CamelCase.js
@@ -0,0 +1,19 @@
+/*
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+*/
+
+
+function solution(string) {
+    return string.replace(/[A-Z]/g, (match) => ' ' + match);
+}
+
+
+console.log(solution('camelCasing'));
+console.log(solution('identifier'));
