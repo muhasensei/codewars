e969c1ff5164f9bdcd1a8a5ff7d68938534598a9
muha.io <zhanmuha01@gmail.com>
2021-12-04 23:48:51 +0600

    added non-repeating 5lvl problem

---
+++ 5kyu/NonRepeating.js
@@ -0,0 +1,37 @@
+/*
+Write a function named first_non_repeating_letter that takes a string input,
+ and returns the first character that is not repeated anywhere in the string.
+
+For example, if given the input 'stress', the function should return 't', 
+since the letter t only occurs once in the string, and occurs first in the string.
+
+As an added challenge, upper- and lowercase letters are considered the same character, 
+but the function should return the correct case for the initial letter. 
+For example, the input 'sTreSS' should return 'T'.
+
+If a string contains all repeating characters, 
+it should return an empty string ("") or None -- see sample tests.
+
+Sample Tests:
+
+describe('Simple Tests', function() {
+  it('should handle simple tests', function() {
+    Test.assertEquals(firstNonRepeatingLetter('a'), 'a');
+    Test.assertEquals(firstNonRepeatingLetter('stress'), 't');
+    Test.assertEquals(firstNonRepeatingLetter('moonmen'), 'e');
+  });
+});
+*/
+
+function firstNonRepeatingLetter(s) {
+    return s
+    .split('')
+    .find(
+        (curr) => {
+            const reg = new RegExp(`${curr}`, 'i');
+            return !s.replace(reg, '').match(reg);
+        }
+    ) || '';   
+}
+
+console.log(firstNonRepeatingLetter('stress'));
